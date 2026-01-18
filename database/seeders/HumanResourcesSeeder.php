dd61871a1742dc68226cc29c0b43def34fc2fa61
unknown <alhiepedia@gmail.com>
2026-01-18 15:30:10 +0800

    installing dashboard template mazer

--- database/seeders/HumanResourcesSeeder.php
+++ database/seeders/HumanResourcesSeeder.php
@@ -5,8 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
-use Carbon\Carbon;
+use Carbon\Carbon; // Tambahkan ini agar Carbon jalan
+use Faker\Factory as Faker; // Tambahkan ini agar Faker jalan
 
 
 class HumanResourcesSeeder extends Seeder
